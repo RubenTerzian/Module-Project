ec0ebf52118d0da235a7d500d452a6a362095f90
Ruben <rbr2063@gmail.com>
2020-11-28 20:27:54 +0200

    add slider

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        speed: 400,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+      });
+  });
